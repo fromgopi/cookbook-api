5f5cbdb1156005d4a63d618d42d9efb8fe4cc7c9
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-03-26 21:54:32 +0530

    Adding strings into resource folders
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

---
+++ src/resources/strings/configuration/__init__.py
@@ -0,0 +1,8 @@
+"""Configuration manager strings"""
+
+# .env
+ENV_FILE_NOT_FOUND = '.env configuration not found.'
+
+# AWS Secrets Manager
+AWS_REGION_NAME_NOT_FOUND = 'Please provide a region name.'
+AWS_SECRET_NAME_NOT_FOUND = 'Please specify a secret name.'
