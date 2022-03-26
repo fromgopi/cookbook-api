0c58cace2fe048ca1bf048283bedd687161048cd
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-03-26 21:47:13 +0530

    Renaming helper filename and adding more functions
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

---
+++ src/common/transformers.py
@@ -0,0 +1,10 @@
+"""Transformers"""
+from marshmallow import Schema
+from src.common.helpers import to_camel_case
+
+
+class CamelCaseSchema(Schema):
+    """Transforms request and response keys from snake_case to camelCase"""
+
+    def on_bind_field(self, field_name, field_obj):
+        field_obj.data_key = to_camel_case(field_obj.data_key or field_name)
