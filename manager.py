c3e348f90bfe1391603b5e0e098999f5bf900d60
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-03-28 22:27:52 +0530

    adding some json logging to repo
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- manager.py
+++ manager.py
@@ -2,3 +2,18 @@
 import os
 from logging import getLogger
 from flask_script import Manager
+
+from src.server import create_app
+
+APP = create_app()
+APP.app_context().push()
+
+MANAGER = Manager()
+
+LOGGER = getLogger(__name__)
+
+@MANAGER.command
+def run():
+    """Development Init"""
+    port = os.getenv('PORT')
+    enable_debug = (os.getenv('FLASK_DEBUG') == 'True')
