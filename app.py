459e2d18ae1b21c7e524cdb5d38547090c03bac7
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-03-31 21:01:54 +0530

    adding some json logging to repo
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
+import os
+from src.common.constants import DEFAULT_PORT
+from src.configuration.modules.logger import RequestHandlerLoggerOverride
 from src.server import create_app
 
-app = create_app()
+def create_instance():
+    """Development Init"""
+    app = create_app()
+    port = os.getenv('PORT')
+    enable_debug = (os.getenv('FLASK_DEBUG') == 'True')
+    port = int(port) if port else DEFAULT_PORT
+    app.run(
+        host='0.0.0.0',
+        port=port,
+        debug=enable_debug,
+        use_reloader=enable_debug,
+        threaded=True,
+        request_handler=RequestHandlerLoggerOverride
+    )
+    
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
-    
+    create_instance()
--- manager.py
+++ manager.py
@@ -1,7 +1,11 @@
 """ Development task runner"""
 import os
 from logging import getLogger
+from unittest import main
 from flask_script import Manager
+from src.common.constants import DEFAULT_PORT
+
+from src.configuration.modules.logger import RequestHandlerLoggerOverride
 
 from src.server import create_app
 
@@ -17,3 +21,16 @@ def run():
     """Development Init"""
     port = os.getenv('PORT')
     enable_debug = (os.getenv('FLASK_DEBUG') == 'True')
+    port = int(port) if port else DEFAULT_PORT
+    
+    APP.run(
+        host='0.0.0.0',
+        port=port,
+        debug=enable_debug,
+        use_reloader=enable_debug,
+        threaded=True,
+        request_handler=RequestHandlerLoggerOverride
+    )
+    
+if __name__ == '__main__':
+    MANAGER.run()
