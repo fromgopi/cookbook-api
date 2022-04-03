8680b3af6dc69ba8ae420010fb363128b4d6876a
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-04-03 21:35:28 +0530

    adding some react stuff
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

---
+++ frontend/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const ENTRY_POINT = path.resolve(__dirname, 'src/index.js')
+const DIST_FOLDER = path.resolve(__dirname, 'dist')
+module.exports = {
+	entry: ENTRY_POINT,
+	output: {
+		path: DIST_FOLDER
+	},
+	devServer: {
+		publicPath: '/',
+		contentBase: './dist',
+		hot: true,
+		open: true,
+		watchOptions: {
+			
+		}
+	}
+};
