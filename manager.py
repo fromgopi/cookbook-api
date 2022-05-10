bf396bb651aeccd823e91d7efcecd67541ff0f09
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-05-10 22:22:43 +0530

    Update manager.py

--- manager.py
+++ manager.py
@@ -4,7 +4,7 @@ from logging import getLogger
 from unittest import main
 from flask_script import Manager
 from src.common.constants import DEFAULT_PORT    
-         
+        
 from src.configuration.modules.logger import RequestHandlerLoggerOverride   
 
 from src.server import create_app 
