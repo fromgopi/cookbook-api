2e41733b2a8a6db0eaa32269770544dbf941f9c1
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-05-12 21:29:31 +0530

    adding some changes into working dir
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/app/user/services/user_service.py
+++ src/app/user/services/user_service.py
@@ -17,14 +17,3 @@ class UserService:
         }
         return response.success(http_status.OK, resource_name='users', resource_data=users, meta=None)
     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
-     
