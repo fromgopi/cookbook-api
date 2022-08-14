72ab252baf3e33049198e892dbeb84c7f51e13e8
Gopi Krishna M <gopi.muktevi@gmail.com>
2022-08-14 10:23:48 +0530

    removing unwanted imports
    Signed-off-by: Gopi Krishna M <gopi.muktevi@gmail.com>

--- src/resources/strings/configuration/__init__.py
+++ src/resources/strings/configuration/__init__.py
@@ -6,4 +6,3 @@ ENV_FILE_NOT_FOUND = '.env configuration not found.'
 # AWS Secrets Manager
 AWS_REGION_NAME_NOT_FOUND = 'Please provide a region name.'
 AWS_SECRET_NAME_NOT_FOUND = 'Please specify a secret name.'
-
